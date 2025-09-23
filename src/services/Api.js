dfd2b187b75f2fd6f114530c1913729998e42eda
Ahmad Fauzi <uzif8100@gmail.com>
2025-09-23 18:31:27 +0700

    test Deploy

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // baseURL: 'http://backend-shop.test/api',
-  baseURL: 'http://str.ahzi.my.id/api',
+  baseURL: 'https://str.ahzi.my.id/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
